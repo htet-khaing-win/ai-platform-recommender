be95dbc3fa6174a299921b5cf4860f694257507c
Htet Khaing Win <p.htetkhaingwin@gmail.com>
2025-08-16 22:07:27 +0800

    Fixed Syntax error and added Tool section

--- backend/app/database/schemas.py
+++ backend/app/database/schemas.py
@@ -30,8 +30,8 @@ class WorkflowRequest(BaseModel):
 
 class ToolResponse(BaseModel):
      name: str
-     description: Optional [str] = None
-     website: Optional [str] = None
+     description: Optional[str] = None
+     website: Optional[str] = None
 
      class Config:
          orm_mode = True
@@ -47,9 +47,28 @@ class WorkflowStepResponse(BaseModel):
 class WorkflowResponse(BaseModel):
     workflow_id: int
     name: str
-    description: Optional [str] = None
+    description: Optional[str] = None
     steps: List[WorkflowStepResponse]
 
     class Config:
         orm_mode = True
+
+class ToolBase(BaseModel):
+    name: str
+    description: str
+
+
+class ToolCreate(ToolBase):
+    pass
+
+class ToolUpdate(BaseModel):
+    name = Optional[str] = None
+    description = Optional[str] = None
+
+class ToolResponse(ToolBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+    
 
